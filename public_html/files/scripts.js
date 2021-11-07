8e55584715eb73ce4d66cf07ad23172d750d669f
Daniel Gaspar <danielfsngaspar@gmail.com>
2021-11-07 13:48:12 +0000

    further changes to page

--- public_html/files/scripts.js
+++ public_html/files/scripts.js
@@ -1,8 +1,18 @@
 function openTab(tabName) {
   var i;
-  var x = document.getElementsByClassName("tab");
+  const x = document.getElementsByClassName("tab");
+  const y = document.getElementById(tabName);
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    if(x[i].id != tabName) { x[i].style.display = "none"; }
   }
-  document.getElementById(tabName).style.display = "block";  
+  if (y.style.display == "none") { y.style.display = "block"; }
+  else { y.style.display = "none"; }
+}
+
+function toggleRuleDisplay(ruleName) {
+  const x = document.getElementById(ruleName)
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  }
+  else { x.style.display = "none"; }
 }
