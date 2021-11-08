a7f8406762361f09c23dfa78434b2967451cd815
Daniel Gaspar <danielfsngaspar@gmail.com>
2021-11-08 12:04:03 +0000

    more changes

--- public_html/files/scripts.js
+++ public_html/files/scripts.js
@@ -1,4 +1,4 @@
-function openTab(tabName) {
+/*function openTab(tabName) {
   var i;
   const x = document.getElementsByClassName("tab");
   const y = document.getElementById(tabName);
@@ -7,6 +7,24 @@ function openTab(tabName) {
   }
   if (y.style.display == "none") { y.style.display = "block"; }
   else { y.style.display = "none"; }
+}*/
+
+/*function openTab(tabName)
+{
+    const select = document.getElementById(tabName);
+    if(select.classList.contains('active'))
+    {
+        select.className = select.className.replace( /(?:^|\s)active(?!\S)/g , '' );       
+    }
+       else
+    {
+        select.className = "active";       
+    }
+}*/
+
+function openTab(tabName) { 
+  const x = document.getElementById(tabName);
+  x.classList.toggle('open');
 }
 
 function toggleRuleDisplay(ruleName) {
@@ -15,4 +33,14 @@ function toggleRuleDisplay(ruleName) {
     x.style.display = "block";
   }
   else { x.style.display = "none"; }
+}
+
+function animateFloatingLoginButton() {
+  const x = document.getElementById("floatingLoginButton");
+  x.classList.toggle('open');
+}
+
+function slideMenu(){
+  $('.navbar').toggleClass('open');
+  /*animation for slide down menu*/
 }
