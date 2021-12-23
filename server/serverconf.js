0bbe47fd841f1e30c8f2b9d18e407bda5791f013
Daniel Gaspar <danielfsngaspar@gmail.com>
2021-12-23 13:24:56 +0000

    added errormessages and other central variables as exports

--- server/serverconf.js
+++ server/serverconf.js
@@ -11,3 +11,20 @@ module.exports.mediaTypes = {
     'jpeg':     'image/jpeg',
     'jpg':      'image/jpeg'
 };
+
+module.exports.errormessages = {
+    arguments: "Invalid Arguments",
+    login: "User registered with a different password",
+    notuser: "This nick isn't registered",
+    invalidconfig: "This group has a different game configuration",
+    overflow: "There are already two players for this group",
+    invalidgame: "Invalid game reference",
+    notturn: "Not your turn to play",
+    invalidmove: "invalid move"
+}
+
+module.exports.rankings = {};
+module.exports.users = {};
+module.exports.rankingsFile = "./storage/rankings.txt";
+module.exports.usersFile = "./storage/users.txt";
+module.exports.groups = {};
