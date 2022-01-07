4a0b1a397dddde5f3fa89bf782012a2b35733463
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-01-07 20:02:40 +0000

    changed join. completed notify and leave

--- mancala/server/modules/playerInputs.js
+++ mancala/server/modules/playerInputs.js
@@ -3,15 +3,18 @@
 const crypto = require("crypto");
 const errormessages = require("./serverconf.js").errormessages;
 
-const groups = require("./serverconf.js").groups;
+const games = require("./serverconf.js").games;
+const { gameMove } = require("./gamelogic.js");
 
 function join(reqBody) {
-  if (Object.keys(reqBody).length == 5 &&
+  if (
+    Object.keys(reqBody).length == 5 &&
     "nick" in reqBody &&
     "password" in reqBody &&
     "size" in reqBody &&
     "initial" in reqBody &&
-    "group" in reqBody) {
+    "group" in reqBody
+  ) {
     const nick = reqBody.nick;
     const password = reqBody.password;
     const size = reqBody.size;
@@ -20,29 +23,32 @@ function join(reqBody) {
 
     if (nick in users) {
       if (encryption.verify(users[nick], password)) {
-        if (group in groups) {
-          if (groups[group].players.length == 1) {
-            if (groups[group].size == size &&
-              groups[group].initial == initial) {
-              groups[group].players.push(nick);
-            } else {
-              return { error: errormessages["invalidconfig"] };
-            }
+        const gamehash = crypto
+          .createHash("md5")
+          .update(JSON.stringify({ initial, size, group }))
+          .digest("hex");
+        if (gamehash in games) {
+          if (games[gamehash].players.length == 1) {
+            games[gamehash].players.push(nick);
+            const player1 = games[gamehash].players[0];
+            const player2 = games[gamehash].players[1];
+            games[gamehash].sides[player1].store = 0;
+            games[gamehash].sides[player1].pits = Array(size).fill(initial);
+            games[gamehash].sides[player2].store = 0;
+            games[gamehash].sides[player2].pits = Array(size).fill(initial);
+            games[gamehash].turn =
+              games[gamehash].players[Math.round(Math.random())];
           } else {
             return { error: errormessages["overflow"] };
           }
         } else {
-          groups[group] = {
+          games[gamehash] = {
             players: [].push(nick),
             size,
             initial,
-            game: crypto
-              .createHash("md5")
-              .update(parseToInt(group))
-              .digest("hex"),
           };
         }
-        return { game: groups[group].game };
+        return { game: gamehash };
       } else {
         return { error: errormessages["login"] };
       }
@@ -54,11 +60,78 @@ function join(reqBody) {
   }
 }
 
-function notify(reqBody){
-
+function notify(reqBody) {
+  if (
+    Object.keys(reqBody).length == 4 &&
+    "nick" in reqBody &&
+    "password" in reqBody &&
+    "game" in reqBody &&
+    "move" in reqBody
+  ) {
+    const nick = reqBody.nick;
+    const password = reqBody.password;
+    const game = reqBody.game;
+    const move = reqBody.move;
+    if (nick in users) {
+      if (encryption.verify(users[nick], password)) {
+        if (game in games) {
+          if (games[game].turn == nick) {
+            if (move >= 0 && move < games[game].size) {
+              gameMove(game,nick,move);
+              return {};
+            } else {
+              return { error: errormessages["invalidmove"] };
+            }
+          } else {
+            return { error: errormessages["notturn"] };
+          }
+        } else {
+          return { error: errormessages["invalidgame"] };
+        }
+      } else {
+        return { error: errormessages["login"] };
+      }
+    } else {
+      return { error: errormessages["notuser"] };
+    }
+  } else {
+    return { error: errormessages["arguments"] };
+  }
 }
 
 function leave(reqBody) {
+  if (
+    Object.keys(reqBody).length == 3 &&
+    "nick" in reqBody &&
+    "password" in reqBody &&
+    "game" in reqBody
+  ) {
+    const nick = reqBody.nick;
+    const password = reqBody.password;
+    const game = reqBody.game;
+    if (nick in users) {
+      if (encryption.verify(users[nick], password)) {
+        if (game in games) {
+          if (games[game].players.length == 1) {
+            games[game].winner = null;
+          } else
+            games[game].winner =
+              games[game].player[0] == nick
+                ? games[game].player[1]
+                : games[game].player[0];
+          return {};
+        } else {
+          return { error: errormessages["invalidgame"] };
+        }
+      } else {
+        return { error: errormessages["login"] };
+      }
+    } else {
+      return { error: errormessages["notuser"] };
+    }
+  } else {
+    return { error: errormessages["arguments"] };
+  }
 }
 
 module.exports = { join, notify, leave };
