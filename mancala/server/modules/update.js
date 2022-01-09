f04eab2f622e8c40f3200ff3a69f2e6718f983dd
Daniel Gaspar <danielfsngaspar@gmail.com>
2022-01-09 12:33:20 +0000

    added the 3 functions for the updater

--- mancala/server/modules/update.js
+++ mancala/server/modules/update.js
@@ -1,6 +1,33 @@
 "use strict";
 
+const responses = {};
+
 const errormessages = require("./serverconf.js").errormessages;
 
-const groups = require("./serverconf.js").groups;
 const games = require("./serverconf.js").games;
+
+function remember(response, gamehash) {
+  if (!(gamehash in responses)) responses[gamehash] = [];
+  responses[gamehash].push(response);
+}
+
+function forget(response, gamehash) {
+  const pos = responses[gamehash].findIndex((resp) => resp === response);
+  if (pos > -1) responses[gamehash].splice(pos, 1);
+  if (gamehash in games) delete games[gamehash];
+}
+
+function update(gamehash) {
+  for (let response of responses[gamehash]) {
+    const data = {};
+    if ("sides" in games[gamehash] && "turn" in games[gamehash]) {
+      data.board = { sides: games[gamehash].sides, turn: games[gamehash].turn };
+    }
+    if ("winner" in games[gamehash]) {
+      data.winner = games[gamehash].winner;
+    }
+    response.write("data: " + JSON.stringify(data) + "\n\n");
+  }
+}
+
+module.exports = { remember, forget, update };
